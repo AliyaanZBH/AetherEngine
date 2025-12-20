c3096e3674f6bc278fc4397000d2c128b81507ab
Aliyaan <93915104+AliyaanZBH@users.noreply.github.com>
2025-12-20 19:26:53 +0000

    cleaned up some warnings

--- AetherEngine/RenderingEngine/Renderer/DirectX/include/RendererDX12.h
+++ AetherEngine/RenderingEngine/Renderer/DirectX/include/RendererDX12.h
@@ -81,10 +81,10 @@ namespace Aether
 		{
 			ID3D12DescriptorHeap* Heap = nullptr;
 			D3D12_DESCRIPTOR_HEAP_TYPE  HeapType = D3D12_DESCRIPTOR_HEAP_TYPE_NUM_TYPES;
-			D3D12_CPU_DESCRIPTOR_HANDLE HeapStartCpu;
-			D3D12_GPU_DESCRIPTOR_HANDLE HeapStartGpu;
-			UINT                        HeapHandleIncrement;
-			ImVector<int>               FreeIndices;
+			D3D12_CPU_DESCRIPTOR_HANDLE HeapStartCpu = {};
+			D3D12_GPU_DESCRIPTOR_HANDLE HeapStartGpu = {};
+			UINT                        HeapHandleIncrement = {};
+			ImVector<int>               FreeIndices = {};
 
 			void Create(ID3D12Device* device, ID3D12DescriptorHeap* heap)
 			{
